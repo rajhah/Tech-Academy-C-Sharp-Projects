a6fdeccc27d12cb7c1b5bd6245591987a9f2482c
rajhah <109645238+rajhah@users.noreply.github.com>
2023-01-04 21:05:14 -0600

    added insurance authorization project

---
+++ insuranceApproval/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace insuranceApproval
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI? (true/false)");
+            bool dui = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("How many speeding tickets do you have?");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Applicant qualified: " + (age > 15 && !dui && tickets < 4));
+            Console.ReadLine();
+        }
+    }
+}
