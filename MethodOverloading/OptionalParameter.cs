6f183bb904dbbad4767cc105dc6547c7cb2a67f3
rajhah <109645238+rajhah@users.noreply.github.com>
2023-01-09 11:26:16 -0600

    Added overloading with optional parameter to the MethodOverloading project

---
+++ MethodOverloading/OptionalParameter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading
+{
+    internal class OptionalParameter
+    {
+        public int MultiplyTogether(int n1, int n2 = 9) { return n1 * n2; }
+
+    }
+}
--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -10,6 +10,11 @@ namespace MethodOverloading
     {
         static void Main(string[] args)
         {
+
+            /*
+             * MAIN METHOD SUBMISSION ASSIGNMENT
+             */
+
             Overloader o = new Overloader();
 
             Console.WriteLine("Enter a number to multiply by 9:");
@@ -21,6 +26,27 @@ namespace MethodOverloading
             Console.WriteLine("Rounded, then multiplied: " + o.MultiplyNine(n2));
             Console.WriteLine("Multiplied, then rounded: " + o.MultiplyNine(n));
             Console.WriteLine("Stringified, multiplied : " + o.MultiplyNine(s));
+
+            Console.ReadLine();
+
+
+            /*
+             *  METHOD SUBMISSION ASSIGNMENT
+             */
+            OptionalParameter op = new OptionalParameter();
+
+            Console.WriteLine("Enter the first integer:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second integer (optional):");
+            try
+            {
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Multiplied together your numbers equal: " + op.MultiplyTogether(num1, num2));
+            }
+            catch
+            {
+                Console.WriteLine("Multiplied by 9 your number equals: " + op.MultiplyTogether(num1));
+            }
 
             Console.ReadLine();
         }
