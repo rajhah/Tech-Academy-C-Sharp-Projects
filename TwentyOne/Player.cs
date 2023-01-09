40e0b843a412b4a9ebf862b9d3315af00f55c282
rajhah <109645238+rajhah@users.noreply.github.com>
2023-01-09 15:22:23 -0600

    Added operator overloads for Game class

--- TwentyOne/Player.cs
+++ TwentyOne/Player.cs
@@ -12,5 +12,18 @@ namespace TwentyOne
         public int Balance { get; set; }
         public string Name { get; set; }
         public bool IsActivelyPlaying { get; set; }
+
+        public static Game operator+ (Game game, Player player)
+        {
+            game.Players.Add(player);
+            return game;
+        }
+
+        public static Game operator- (Game game, Player player)
+        {
+            game.Players.Remove(player);
+            return game;
+        }
+
     }
 }
--- TwentyOne/Program.cs
+++ TwentyOne/Program.cs
@@ -11,16 +11,20 @@ namespace TwentyOne
         static void Main(string[] args)
         {
 
-            Game game= new TwentyOneGame();
+            Game game = new TwentyOneGame();
+            game.Players = new List<Player>();
+            Player player = new Player
+            {
+                Name = "Phil"
+            };
 
+            game += player;
+            game -= player;
+            
             Deck deck = new Deck();
             deck.Shuffle(8);
 
-            foreach(Card card in deck.Cards)
-            {
-                Console.WriteLine(card.Face + " of " + card.Suit);
-            }
-            Console.WriteLine(deck.Cards.Count);
+            Console.WriteLine("Players: " + game.Players.Count());
 
             Console.ReadLine();
         }
