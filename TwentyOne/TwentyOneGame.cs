8bb684afb97c51a27f7ce4998cd48a4c1f84b2ab
rajhah <109645238+rajhah@users.noreply.github.com>
2023-01-09 12:52:46 -0600

    Added interface IWalkAway

--- TwentyOne/TwentyOneGame.cs
+++ TwentyOne/TwentyOneGame.cs
@@ -6,9 +6,14 @@ using System.Threading.Tasks;
 
 namespace TwentyOne
 {
-    internal class TwentyOneGame : Game
+    internal class TwentyOneGame : Game, IWalkAway
     {
         public override void Play()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WalkAway(Player player)
         {
             throw new NotImplementedException();
         }
