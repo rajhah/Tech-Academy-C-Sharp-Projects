59c22adee77d9dd9b5f3f949d4474b159d5492cc
rajhah <109645238+rajhah@users.noreply.github.com>
2023-01-09 11:14:21 -0600

    Added MethodOverloading pfoject

---
+++ MethodOverloading/Overloader.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading
+{
+    internal class Overloader
+    {
+        public int MultiplyNine(int n)
+        {
+            return (n * 9);
+        }
+
+        public int MultiplyNine(decimal n)
+        {
+            return (int)(n * 9);
+        }
+
+        public int MultiplyNine(string s)
+        {
+            int n = Convert.ToInt32(s);
+            return (n * 9);
+        }
+
+    }
+}
---
+++ MethodOverloading/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Overloader o = new Overloader();
+
+            Console.WriteLine("Enter a number to multiply by 9:");
+            
+            decimal n = Convert.ToDecimal(Console.ReadLine());
+            int n2 = Convert.ToInt32(n);
+            string s = n2.ToString();
+
+            Console.WriteLine("Rounded, then multiplied: " + o.MultiplyNine(n2));
+            Console.WriteLine("Multiplied, then rounded: " + o.MultiplyNine(n));
+            Console.WriteLine("Stringified, multiplied : " + o.MultiplyNine(s));
+
+            Console.ReadLine();
+        }
+    }
+}
