af6e71dbde8bac4659b643f19711a72e5dbd0792
rajhah <109645238+rajhah@users.noreply.github.com>
2023-01-09 10:45:04 -0600

    Added CallingMethodsConsoleApp to demonstrate calling methods from a different class

---
+++ CallingMethodsConsoleApp/Hash.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsConsoleApp
+{
+    public class Hash
+    {
+        public int Reverse(int i)
+        {
+            char[] charArray = i.ToString().ToCharArray();
+            Array.Reverse(charArray);
+            string back = new string(charArray);
+            i = Convert.ToInt32(back);
+            return i;
+        }
+
+        public int SquareRoot(int i)
+        {
+            return ((int)Math.Sqrt(i));
+        }
+
+        public int Multiply(int i)
+        {
+            return (i * 2);
+        }
+
+    }
+}
---
+++ CallingMethodsConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hash h = new Hash();
+            Console.WriteLine("Please enter a whole number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your number doubled is: " + h.Multiply(num));
+            Console.WriteLine("Your number reversed is: " + h.Reverse(num));
+            Console.WriteLine("The square root of your number is close to: " + h.SquareRoot(num));
+            Console.ReadLine();
+        }
+    }
+}
