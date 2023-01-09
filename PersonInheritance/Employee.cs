4c0654de8637b2ee69b63da69c36c3435fcdcf45
rajhah <109645238+rajhah@users.noreply.github.com>
2023-01-09 13:52:40 -0600

    Added IQuittable interface to Employee inheritance

--- PersonInheritance/Employee.cs
+++ PersonInheritance/Employee.cs
@@ -6,14 +6,20 @@ using System.Threading.Tasks;
 
 namespace PersonInheritance
 {
-    internal class Employee: Person
+    internal class Employee: Person, IQuittable
     {
         public int Id { get; set; }
+        public string Job { get; set; }
 
         public override void SayName()
         {
             Console.WriteLine("Employee Name: ");
             base.SayName();
+        }
+
+        public void Quit()
+        {
+            Job = "None";
         }
     }
 }
--- PersonInheritance/Program.cs
+++ PersonInheritance/Program.cs
@@ -10,9 +10,17 @@ namespace PersonInheritance
     {
         static void Main(string[] args)
         {
-            Employee employee = new Employee { FirstName = "Sample", LastName = "Student" };
+            Employee employee = new Employee { FirstName = "Sample", LastName = "Student", Job = "Student" };
             employee.SayName();
+            Console.WriteLine("Job: " + employee.Job);
+            
+            IQuittable quittable = new Employee { FirstName = "Sample", LastName = "Worker", Job = "Generic Job" };
+            Console.WriteLine("Job: " + quittable.Job);
+            quittable.Quit();
+            Console.WriteLine(quittable.Job);
             Console.ReadLine();
+
+
         }
     }
 }
