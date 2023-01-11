307cbd92e0a43b6c8c00fdbb2c4eb0a76b03922d
rajhah <109645238+rajhah@users.noreply.github.com>
2023-01-11 12:47:13 -0600

    Added TimeAfterConsoleApp project. Added additional logging to Dealer class.

---
+++ TimeAfterConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeAfterConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime time= DateTime.Now;
+            Console.WriteLine("The current date and time is: " + time);
+            Console.WriteLine("Please enter a whole number: ");
+            int hours = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("In {0} hours, the date and time will be {1}.", hours, time.AddHours(hours));
+            Console.ReadLine();
+        }
+    }
+}
