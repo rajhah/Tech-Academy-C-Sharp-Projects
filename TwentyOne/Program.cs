f0d749d3cbd957412b7bacafb30a3d6759f765d7
rajhah <109645238+rajhah@users.noreply.github.com>
2023-01-13 17:14:05 -0600

    Added some exception handling

--- TwentyOne/Program.cs
+++ TwentyOne/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,8 +14,30 @@ namespace TwentyOne
         {
             Console.WriteLine("Welcome to the Grand Hotel and Casino. Let's start by telling me your name:");
             string playerName = Console.ReadLine();
-            Console.WriteLine("And how much money did you bring today?");
-            int bank = Convert.ToInt32(Console.ReadLine());
+
+            if (playerName.ToLower() == "admin")
+            {
+                List<ExceptionEntity> Exceptions = ReadExceptions();
+                foreach(var e in Exceptions)
+                {
+                    Console.Write(e.Id + " | ");
+                    Console.Write(e.ExceptionType + " | ");
+                    Console.Write(e.ExceptionMessage + " | ");
+                    Console.Write(e.TimeStamp + " | ");
+                    Console.WriteLine();
+                }
+                Console.ReadLine();
+                return;
+            }
+
+            bool validAnswer = false;
+            int bank = 0;
+            while (!validAnswer)
+            {
+                Console.WriteLine("And how much money did you bring today?");
+                validAnswer = int.TryParse(Console.ReadLine(), out bank);
+                if (!validAnswer) Console.WriteLine("Please enter a whole number.");
+            }
             Console.WriteLine("Hello {0}. Would you like to join a game of 21 right now? (y/n)", playerName);
             string answer = Console.ReadLine().ToLower();
             if (answer == "yes" || answer == "y")
@@ -24,7 +48,24 @@ namespace TwentyOne
                 player.IsActivelyPlaying = true;
                 while (player.IsActivelyPlaying && player.Balance > 0)
                 {
-                    game.Play();
+                    try
+                    {
+                        game.Play();
+                    }
+                    catch (FraudException e)
+                    {
+                        Console.WriteLine(e.Message);
+                        UpdateDbWithException(e);
+                        Console.ReadLine();
+                        return;
+                    }
+                    catch(Exception e) 
+                    {
+                        Console.WriteLine("An error occured.");
+                        UpdateDbWithException(e);
+                        Console.ReadLine();
+                        return;
+                    }
                 }
                 game -= player;
                 Console.WriteLine("Thank you for playing!");
@@ -33,5 +74,58 @@ namespace TwentyOne
             Console.WriteLine("Feel free to look around the casino. Bye for now!");
             Console.ReadLine();
         }
+
+        private static List<ExceptionEntity> ReadExceptions()
+        {
+            string connectionString = @"Data Source=(localdb)\ProjectModels;Initial Catalog=TwentyOneGame;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            string queryString = @"SELECT Id, ExceptionType, ExceptionMessage, Timestamp FROM Exceptions";
+
+            List<ExceptionEntity> Exceptions = new List<ExceptionEntity>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    ExceptionEntity exception= new ExceptionEntity();
+                    exception.Id = Convert.ToInt32(reader["Id"]);
+                    exception.ExceptionType = reader["ExceptionType"].ToString();
+                    exception.ExceptionMessage = reader["ExceptionMessage"].ToString();
+                    exception.TimeStamp = Convert.ToDateTime(reader["TimeStamp"]);
+                    Exceptions.Add(exception);
+                }
+                connection.Close();
+            }
+
+            return Exceptions;
+        }
+
+        private static void UpdateDbWithException (Exception e)
+        {
+            string connectionString = @"Data Source=(localdb)\ProjectModels;Initial Catalog=TwentyOneGame;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            string queryString = "INSERT INTO Exceptions (ExceptionType, ExceptionMessage, Timestamp)" +
+                                    "VALUES (@ExceptionType, @ExceptionMessage, @TimeStamp)";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.Parameters.Add("@ExceptionType", SqlDbType.VarChar);
+                command.Parameters.Add("@ExceptionMessage", SqlDbType.VarChar);
+                command.Parameters.Add("@TimeStamp", SqlDbType.DateTime);
+
+                command.Parameters["@ExceptionType"].Value = e.GetType().ToString();
+                command.Parameters["@ExceptionMessage"].Value = e.Message;
+                command.Parameters["@TimeStamp"].Value = DateTime.Now;
+
+                connection.Open();
+                command.ExecuteNonQuery();
+                connection.Close();
+            }
+
+        }
+
     }
 }
